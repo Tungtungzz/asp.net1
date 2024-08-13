6c2b985f565c917d44411552332716f2e9f4606e
Tungtungzz <90121922+Tungtungzz@users.noreply.github.com>
2024-08-13 13:33:34 +0700

    Add files via upload

--- KiemTra/Migrations/20240813051046_CreateTable.Designer.cs
+++ KiemTra/Migrations/20240813051046_CreateTable.Designer.cs
@@ -3,6 +3,7 @@ using KiemTra.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KiemTra.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240813051046_CreateTable")]
+    partial class CreateTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
